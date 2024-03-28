1921294da6626ae77fb32cc38ab4a3b8c028954e
Lewin Probst <info@emirror.de>
2024-03-28 12:04:57 +0100

    chore: Ticket is now using serde instead of rocket::serde

--- src/ticket.rs
+++ src/ticket.rs
@@ -1,11 +1,10 @@
-use rocket::serde::{
+use serde::{
     Deserialize,
     Serialize,
 };
 
 /// Contains the credentials for a simple login.
 #[derive(Serialize, Deserialize, Debug)]
-#[serde(crate = "rocket::serde")]
 pub struct Ticket {
     /// The identification of the user, eg. a username.
     pub id: String,
