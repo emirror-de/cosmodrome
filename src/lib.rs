97dd8f3d5fcc52a056bc1a32efcfa7a303161d1d
Lewin Probst <info@emirror.de>
2024-08-24 08:32:33 +0200

    doc: Updated html logo and favicon

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![deny(missing_docs)]
 #![feature(doc_cfg)]
+#![doc(
+    html_logo_url = "https://github.com/emirror-de/cosmodrome/blob/unstable/resources/icon.png?raw=true",
+    html_favicon_url = "https://github.com/emirror-de/cosmodrome/blob/unstable/resources/icon.png?raw=true"
+)]
 #![doc = include_str!("../README.md")]
 
 #[cfg(feature = "server")]
